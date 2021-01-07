eb77a8f75595b0a2063c4244203a96c4e7d89056
zringele <zilvinas.ringele@gmail.com>
2021-01-07 15:07:31 +0200

    Initial commit

---
+++ sender.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Sender.net email marketing
+   Plugin URI: https://sender.net
+   description: Sender email marketing tool
+   Version: 2.0
+   Author: Sender
+   Author URI: https://sender.net
+   License: GPL2
+   */
+?>
