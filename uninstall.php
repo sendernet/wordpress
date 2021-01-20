2b5ef7e2078b71778d9763bd298a70d1ce8bfd75
gytis <gytis@sender.net>
2021-01-20 10:42:52 +0200

    uninstall php

--- uninstall.php
+++ uninstall.php
@@ -4,10 +4,36 @@
     if (!defined('ABSPATH')) {
         exit;
     }
-    // remove plugin options
 
-    // remove plugin transients
+    $availableSettings = [
+        'sender_api_key',
+        'sender_allow_guest_track',
+        'sender_allow_import',
+        'sender_allow_forms',
+        'sender_customers_list',
+        'sender_registration_list',
+        'sender_registration_track',
+        'sender_cart_period',
+        'sender_has_woocommerce',
+        'sender_high_acc',
+        'sender_allow_push',
+    ];
 
-    // remove plugin cron events
+    global $wpdb;
+
+    $tables = [
+      "sender_automated_emails_carts",
+      "sender_automated_emails_users"
+    ];
+
+    foreach ($tables as $table){
+        $name = $wpdb->prefix . $table;
+        $wpdb->query( "DROP TABLE {$name}");
+    }
+
+    foreach ($availableSettings as $setting)
+    {
+        delete_option($setting);
+    }
 
 ?>
