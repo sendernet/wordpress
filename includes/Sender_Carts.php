0d4d919388d531900c37982bcadf2a32fb476f66
dimrak22 <diego@sender.net>
2022-12-29 16:24:48 +0200

    use declared variable

--- includes/Sender_Carts.php
+++ includes/Sender_Carts.php
@@ -265,12 +265,11 @@ class Sender_Carts
 
     public function senderGetVisitor()
     {
-        $visitor = $_COOKIE['sender_site_visitor'];
-        $user = (new Sender_User())->findBy('visitor_id', $visitor);
+        $user = (new Sender_User())->findBy('visitor_id', $this->senderSessionCookie);
 
         if (!$user) {
             $user = new Sender_User();
-            $user->visitor_id = $visitor;
+            $user->visitor_id = $this->senderSessionCookie;
             $user->save();
         }
         return $user;
